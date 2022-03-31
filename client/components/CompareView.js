f0f1f2f2499b4f503757cc34ca2c715ccdb86856
Sandra Magnusdottir <sandrakristrun90@gmail.com>
2022-03-30 20:06:44 -0400

    fix

--- client/components/CompareView.js
+++ client/components/CompareView.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { fetchCities } from "../store/cities";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 import CompareCity from "./CompareCity";
 import {
   getCityByName1,
